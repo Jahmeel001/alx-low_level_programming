dafb1ff3aa6cf20fff430655588b2c55cc601d77
Jahmeel001 <jahmeel001@gmail.com>
2022-09-21 16:31:06 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,8 +7,6 @@
 * works identically to the standard library function `strncpy` .
 *@dest: buffer storing the string copy
 *@src:the source string
-
-
 *@n:max number of byte copied
 *Return: returns
 */
@@ -21,7 +19,7 @@ char *_strncpy(char *dest, char *arc, int n)
 		dest[i] = src[i];
 
 	for ( ; i < n; i++)
-		dest[i] = '\0' ;
+		dest[i] = '\0';
 
 	return (dest);
 }
